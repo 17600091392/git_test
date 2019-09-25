06a177e9443e1a2988979e10c78734ac917d0a97
fuxj <1323699999@qq.com>
2019-09-25 22:14:11 +0800

    员工2 V3.0

--- src/test/java/com/baizhi/git_test/GitTestApplicationTests.java
+++ src/test/java/com/baizhi/git_test/GitTestApplicationTests.java
@@ -14,6 +14,9 @@ public class GitTestApplicationTests {
         System.out.println("打印一行");
         System.out.println("github上添加的内容");
         System.out.println("员工1进行了开发");
+        int a=100;
+        System.out.println("员工2设置了一个变量");
+        System.out.println("员工2 使用技能 打鼾 生命值恢复了");
     }
 
 }
